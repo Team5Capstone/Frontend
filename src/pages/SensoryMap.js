aaae417ff8a4f15d514663a89442ce11c99a3587
Triane Peart <TrianePeart@gmail.com>
2023-05-31 10:54:05 -0400

    Changed files

---
+++ src/pages/SensoryMap.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Rating from '../components/Rating';
+// import Review from '../components/Review';
+import Map from '../components/Map'
+
+function SensoryMap() {
+    return (
+        <div>
+            {/* <Rating />
+            <Review /> */}
+            <Map/>
+        </div>
+    );
+}
+
+export default SensoryMap;
