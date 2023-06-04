85da953022df84fdf109f7c36da2d33e180d8822
MarsiaRochelin <marsrochelin@pursuit.org>
2023-06-04 19:40:19 -0400

    comment tweak

--- src/helperFunctions/helperFuncs.js
+++ src/helperFunctions/helperFuncs.js
@@ -2,7 +2,7 @@ import fourOFourError from "../pages/Four0Four";
 
 export default helperFuncs = () => {
   const fourOFourError = () => {
-    //if route provided isn't one of the routes accounted for
+    //if route provided route isn't one of the routes accounted for
     return (
       <div>
         <fourOFourError />
